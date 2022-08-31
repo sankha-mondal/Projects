8a0e04342d0cbbecf5bdd8f20502042413dc0984
Sankha Subhra Mondal <sankha123@gmail.com>
2022-09-01 00:45:55 +0530

    User Management - Frontend

---
+++ User Management System/Frontend Work/User-Management-System/src/app/Interfaces/user.ts
@@ -0,0 +1,10 @@
+// it is use to map the backend technologies json data 
+export interface User {
+
+    uEmail: string;  // PK
+    uName: string;
+    uPassword: string;
+    uRole: string;
+    url: string;
+ 
+}
